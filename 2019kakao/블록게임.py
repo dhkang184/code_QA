67edabdb9fff8db3885b9d25843edcaf31061b40
dhkang <kon7200@gmail.com>
2019-09-09 15:53:46 +0900

    2019.09.09 업데이트
    업데이트

--- 2019kakao/블록게임.py
+++ 2019kakao/블록게임.py
@@ -1,3 +1,5 @@
+# 2018 카카오 : https://www.welcomekakao.com/learn/courses/30
+
 # https://www.welcomekakao.com/learn/courses/30/lessons/42894
 
 """
@@ -6,56 +8,99 @@ w, h = 8, 5.
 Matrix = [[0 for x in range(w)] for y in range(h)]
 
 """
-
-def check_shape(cur_w, cur_h,max_w, max_h, num, matrix):
-    dw = [[1,2,2], [1,0,0], [0,1,2], [0,0,-1],
-          [1,2,0], [0,0,1], [0,-1,-2],[1,1,1],
-          [-1,0,1], [0,0,1], [1,2,1],[0,0,-1]
-          ]
-    dh = [[0,0,1], [0,1,2], [1,1,1], [1,2,2],
-          [0,0,1], [1,2,2], [1,1,1], [0,1,2],
-          [1,1,1], [1,2,1], [0,0,1], [1,2,1]
-          ]
-    for idx in range(len(dw)):
-        check_count =0
-        for s_idx in range(3):
-            check_w = cur_w + dw[idx][s_idx]
-            check_h = cur_h + dh[idx][s_idx]
-            if check_h >-1 and check_w> -1 and check_h <= max_h and check_w <=max_w:
-                if matrix[check_w][check_h] == num:
-                    check_count += 1
-                    if check_count == 3:
-                        return idx
-                else:
-                    check_count = 0
-                    break
-            else:
-                check_count = 0
-                break
-
-def masking(mask_matrix, num, cur_w, cur_h):
-    max_h = len(mask_matrix) -1
-    if num in [0,4,10] :
-        for idx in range(max_h - cur_h):
-            mask_matrix[cur_w][cur_h+idx] = -1
-            mask_matrix[cur_w+1][cur_h+idx] = -1
-            mask_matrix[cur_w+2][cur_h+idx] = -1
-    if num in [1,7] :
-        for idx in range(max_h - cur_h):
-            mask_matrix[cur_w][cur_h+idx] = -1
-            mask_matrix[cur_w+1][cur_h+idx] = -1
-
+"""
+인터넷 솔루션...
 def solution(board):
+    total_col = len(board[0])
+    total_row = len(board)
     answer = 0
-    h = len(board)
-    w = len(board[0])
-    mask_matrix = [[0 for x in range(w)] for y in range(h)]
-    check_dict = {}
-    for cur_h in range(h):
-        for cur_w in range(w):
-            if board[cur_w][cur_h] >0 and board[cur_w][cur_h] not in check_dict:
-                num = check_shape(cur_w,cur_h,w,h,board[cur_w][cur_h], board)
-                check_dict[num] = [cur_w, cur_h]
+    pre_answer = -1
+    count = 0
+    black_blocks = []
+    while(count <= 2) :
+
+
+        # Set black blocks
+        count += 1
+        for col in range(total_col) :
+            idx = 0
+
+            while(True) :
+
+                if board[0][col] != 0:
+                    break
+
+                if idx >= total_row - 1:
+                    board[idx][col] = -1
+                    black_blocks.append([idx, col])
+                    break
+
+                if board[idx + 1][col] != 0 :
+                    board[idx][col] = -1
+                    black_blocks.append([idx, col])
+                    break
+
+                idx += 1
+
+
+
+        # 2 x 3 block (height x width)
+        for row in range(total_row - 1) :
+            for col in range(total_col - 2) :
+                block_info = []
+                if board[row][col] != 0 :
+                    block_info.append(board[row][col])
+                    block_info.append(board[row][col + 1])
+                    block_info.append(board[row][col + 2])
+                    block_info.append(board[row + 1][col])
+                    block_info.append(board[row + 1][col + 1])
+                    block_info.append(board[row + 1][col + 2])
+
+                block_Set = set(block_info)
+
+                if block_info.count(-1) == 2 and block_info.count(0) == 0 and len(block_Set) == 2:
+                    answer += 1
+                    board[row][col] = 0
+                    board[row][col + 1] = 0
+                    board[row][col + 2] = 0
+                    board[row + 1][col] = 0
+                    board[row + 1][col + 1] = 0
+                    board[row + 1][col + 2] = 0
+
+                    for idx in black_blocks :
+                        x, y = idx
+                        board[x][y] = 0
+                    black_blocks = []
+                    count = 0
+        # 3 x 2 block (height x width)
+        for row in range(total_row - 2) :
+            for col in range(total_col - 1) :
+                block_info = []
+                if board[row][col] != 0 :
+                    block_info.append(board[row][col])
+                    block_info.append(board[row + 1][col])
+                    block_info.append(board[row + 2][col])
+                    block_info.append(board[row][col + 1])
+                    block_info.append(board[row + 1][col + 1])
+                    block_info.append(board[row + 2][col + 1])
+
+                block_Set = set(block_info)
+
+                if block_info.count(-1) == 2 and block_info.count(0) == 0 and len(block_Set) == 2:
+                    answer += 1
+                    board[row][col] = 0
+                    board[row + 1][col] = 0
+                    board[row + 2][col] = 0
+                    board[row][col + 1] = 0
+                    board[row + 1][col + 1] = 0
+                    board[row + 2][col + 1] = 0
+
+                    for idx in black_blocks :
+                        x, y = idx
+                        board[x][y] = 0
+                    black_blocks = []
+                    count = 0
 
 
     return answer
+"""

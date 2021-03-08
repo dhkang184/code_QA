9dfce7a1b65d30b94aac75aad863ea5bca021882
donghyun_Juam_DESK <kon7200@gmail.com>
2021-03-09 00:09:43 +0900

    2021-03-09 프로그래머스 해시
    해시

--- 백준/삼성기출/17822_원판 돌리기.py
+++ 백준/삼성기출/17822_원판 돌리기.py
@@ -13,12 +13,58 @@ for t_idx in range(t):
     xi, di, ki = m_data[t_idx]
     for idx in range(xi-1, n, xi):
         if di == 0:
-            data[idx] = data[idx][-ki:] + data[idx][:-ki]
+            data[idx] = data[idx][-ki:] + data[idx][:(m-ki)]
         elif di == 1:
             data[idx] = data[idx][ki:] + data[idx][:ki]
 
-    new_data = [[INF]*m for _ in range(n)]
+    ext_data = [[INF]*(m+2) for _ in range(n)]
+    for idx in range(n):
+        ext_data[idx] =data[idx][-1:] + data[idx] + data[idx][:1]
+
+    new_data = [[INF]*(m+2) for _ in range(n)]
+    C = True #지울 값이 없는 경우
+    total = 0
+    cnt = 0
     for i in range(n):
         for j in range(m):
-            if i == 0:
-                if new_data[i][j] == new_data[i]
+            if i == 0 :
+                if ext_data[i][j+1] != ext_data[i][j+2] and ext_data[i][j+1] != ext_data[i][j] and ext_data[i][j+1] !=ext_data[i+1][j+1]:
+                    new_data[i][j+1] = ext_data[i][j+1]
+                    total += ext_data[i][j+1]
+                    cnt +=1
+                elif ext_data[i][j+1] != INF:
+                    C = False
+            elif i == n-1:
+                if ext_data[i][j+1] != ext_data[i][j+2] and ext_data[i][j+1] != ext_data[i][j] and ext_data[i][j+1] !=ext_data[i-1][j+1]:
+                    new_data[i][j+1] = ext_data[i][j+1]
+                    total += ext_data[i][j + 1]
+                    cnt += 1
+                elif ext_data[i][j + 1] != INF:
+                    C = False
+            else:
+                if ext_data[i][j+1] != ext_data[i][j+2] and ext_data[i][j+1] != ext_data[i][j] and ext_data[i][j+1] !=ext_data[i-1][j+1] and ext_data[i][j+1] !=ext_data[i+1][j+1]:
+                    new_data[i][j+1] = ext_data[i][j+1]
+                    total += ext_data[i][j + 1]
+                    cnt += 1
+                elif ext_data[i][j + 1] != INF:
+                    C = False
+
+    if C == True and cnt >0:
+        avg = total/cnt
+        for i in range(n):
+            for j in range(m):
+                if new_data[i][j+1] > avg and new_data[i][j+1] != INF:
+                    new_data[i][j+1] -= 1
+                elif new_data[i][j+1] < avg:
+                    new_data[i][j+1] += 1
+
+    data =[[0]*m for _ in range(n)]
+    answer =0
+    for i in range(n):
+        for j in range(m):
+            data[i][j] = new_data[i][j+1]
+            if data[i][j] != INF:
+                answer += data[i][j]
+
+
+print(str(answer))

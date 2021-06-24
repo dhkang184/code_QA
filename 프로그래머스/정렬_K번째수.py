6417638181d1966c089fd7a7267b7dc025ac69ba
donghyun_Juam_DESK <kon7200@gmail.com>
2021-06-24 22:19:09 +0900

    프로그래머스 초기화
    프로그래머스 초기화

--- 프로그래머스/정렬_K번째수.py
+++
@@ -1,8 +0,0 @@
-#https://programmers.co.kr/learn/courses/30/lessons/42748
-#시작시간 23:14 ~ 23:25
-
-def solution(array, commands):
-
-    return [sorted(array[x[0] -1:x[1]])[x[2]-1] for x in commands]
-
-print(solution([1, 5, 2, 6, 3, 7, 4], [[2, 5, 3], [4, 4, 1], [1, 7, 3]]))
